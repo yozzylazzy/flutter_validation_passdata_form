d1bd7a3fc5595e471d84e307287e64cc633b0c1d
Yosef Adrian <kristian_yosef@yahoo.com>
2022-10-01 08:35:18 +0700

    Ner Register Form Validation

---
+++ lib/register_form.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+//create register form statefull
+class RegisterFormStatefull extends StatefulWidget {
+  const RegisterFormStatefull({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterFormStatefull> createState() => _RegisterFormStatefullState();
+}
+
+class _RegisterFormStatefullState extends State<RegisterFormStatefull> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+    );
+  }
+}
